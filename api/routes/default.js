dbb34de6030483fe861f81b037d7db48749c197d
CarsonForsyth <4sythsemail@gmail.com>
2021-03-17 23:33:20 -0400

    user login

--- api/routes/default.js
+++ api/routes/default.js
@@ -29,6 +29,10 @@ app.get("/login", (req, res) => {
   res.render("login.ejs");
 });
 
+app.get("/signup", (req, res) => {
+  res.render("signup.ejs");
+});
+
 app.get("/recipes", (req, res) => {
   console.log("here")
   res.render("viewManyRecipe.ejs");
